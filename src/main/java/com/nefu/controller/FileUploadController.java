85ad506293cd5db13cab6c095c6048c94f7dfad8
Jeffylison <1006522213@qq.com>
2025-04-01 13:46:43 +0800

    Redis and application

--- src/main/java/com/nefu/controller/FileUploadController.java
+++ src/main/java/com/nefu/controller/FileUploadController.java
@@ -1,5 +1,6 @@
 package com.nefu.controller;
 
+import com.nefu.AliOssUtil;
 import com.nefu.pojo.Result;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,14 +14,14 @@ import java.util.UUID;
 public class FileUploadController {
 
     @PostMapping("/upload")
-    public Result<String> upload(MultipartFile file) throws IOException {
+    public Result<String> upload(MultipartFile file) throws Exception {
         //文件存储本地磁盘
         String originalFilename = file.getOriginalFilename();
         //UUID保证文件名称唯一
         String filename = UUID.randomUUID().toString() + originalFilename.substring(originalFilename.lastIndexOf("."));
 
-        file.transferTo(new File("C:\\Users\\10065\\Desktop\\BigEventImg\\" + filename));
-
-        return Result.success("url访问地址...");
+//        file.transferTo(new File("C:\\Users\\10065\\Desktop\\BigEventImg\\" + filename));
+        String url = AliOssUtil.uploadFile(filename, file.getInputStream());
+        return Result.success(url);
     }
 }
